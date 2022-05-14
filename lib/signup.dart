37e383d4b45e99600b3c7b8731b3a802eaca3a75
Sandun jayasekara <jmsrjayasekara97@gmail.com>
2022-05-14 17:38:07 +0530

    Added signup form

--- lib/signup.dart
+++ lib/signup.dart
@@ -8,6 +8,76 @@ class SignupView extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold();
+    return Scaffold(
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: const Text(
+                "REGISTER",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2661FA),
+                    fontSize: 36
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.02),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                    labelText: "Email Address"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.02),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                    labelText: "Name"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.02),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                    labelText: "Password"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.02),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                    labelText: "Confirm password"
+                ),
+                obscureText: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
