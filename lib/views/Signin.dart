eb97edcbc38a71dfe046fc13fdf57c3c76364f7e
Thilakshan <thilakshanlk@gmail.com>
2022-05-14 19:21:46 +0530

    Added signin button

--- lib/views/Signin.dart
+++ lib/views/Signin.dart
@@ -72,7 +72,58 @@ class _SigninViewState extends State<SigninView> {
               ),
             ),
 
+            SizedBox(height: size.height * 0.05),
 
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: const LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 255, 136, 34),
+                            Color.fromARGB(255, 255, 177, 41)
+                          ]
+                      )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: const Text(
+                    "LOGIN",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupView()))
+                },
+                child: const Text(
+                  "Don't Have an Account? Sign up",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2661FA)
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
