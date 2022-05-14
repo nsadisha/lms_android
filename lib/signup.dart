f0cda7441c824d96af5283d59809f5f94acd331b
Sandun jayasekara <jmsrjayasekara97@gmail.com>
2022-05-14 18:51:15 +0530

    Added radio list for user role

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:lms_android/components/background.dart';
 
-class SignupView extends StatelessWidget {
+enum UserRole { student, lecturer }
+
+class SignupView extends StatefulWidget {
   const SignupView({Key? key}) : super(key: key);
+
+  @override
+  State<SignupView> createState() => _SignupViewState();
+}
+
+class _SignupViewState extends State<SignupView> {
+
+  UserRole? _character = UserRole.student;
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +60,68 @@ class SignupView extends StatelessWidget {
                 ),
               ),
             ),
+
+            SizedBox(height: size.height * 0.02),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(top: 10.0),
+                          child: Text('You are a',
+                            style: TextStyle(
+                              fontSize: 16,
+                                color: Colors.black54,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: SizedBox(
+                            height: 30.0,
+                            child: ListTile(
+                              title: const Text('Student'),
+                              leading: Radio<UserRole>(
+                                value: UserRole.student,
+                                groupValue: _character,
+                                onChanged: (UserRole? value) {
+                                  setState(() {
+                                    _character = value;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            height: 30.0,
+                            child: ListTile(
+                              title: const Text('Lecturer'),
+                              leading: Radio<UserRole>(
+                                value: UserRole.lecturer,
+                                groupValue: _character,
+                                onChanged: (UserRole? value) {
+                                  setState(() {
+                                    _character = value;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
 
             SizedBox(height: size.height * 0.02),
 
