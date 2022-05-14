70b5ae37d36fbee4ab2ac7b618434b5167b2fc28
Sandun jayasekara <jmsrjayasekara97@gmail.com>
2022-05-14 18:52:40 +0530

    Added signup button

--- lib/signup.dart
+++ lib/signup.dart
@@ -147,6 +147,59 @@ class _SignupViewState extends State<SignupView> {
                 obscureText: true,
               ),
             ),
+
+            SizedBox(height: size.height * 0.05),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: const LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 255, 136, 34),
+                            Color.fromARGB(255, 255, 177, 41)
+                          ]
+                      )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: const Text(
+                    "SIGN UP",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+
+                },
+                child: const Text(
+                  "Already Have an Account? Sign in",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2661FA)
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
