0902a1b9c9e1019b1e322c9a78d86a351519800d
Sandun jayasekara <jmsrjayasekara97@gmail.com>
2022-05-14 17:24:46 +0530

    Added signup view

---
+++ lib/signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:lms_android/components/background.dart';
+
+class SignupView extends StatelessWidget {
+  const SignupView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold();
+  }
+}
