262b8c1e1bd337fa33999d70b584426e1aaeb304
Thilakshan <thilakshanlk@gmail.com>
2022-05-14 19:21:17 +0530

    Added signin form

--- lib/views/Signin.dart
+++ lib/views/Signin.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lms_android/views/signup.dart';
+
+import '../components/background.dart';
 
 class SigninView extends StatefulWidget {
   const SigninView({Key? key}) : super(key: key);
@@ -10,6 +13,69 @@ class SigninView extends StatefulWidget {
 class _SigninViewState extends State<SigninView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: const Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2661FA),
+                    fontSize: 36
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                    labelText: "Email Address"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: const TextField(
+                decoration: InputDecoration(
+                    labelText: "Password"
+                ),
+                obscureText: true,
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: const Text(
+                "Forgot your password?",
+                style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0XFF2661FA)
+                ),
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
