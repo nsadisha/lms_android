32e7ca180b9e65ed6413a7aa7e098903b95424cc
Sadisha Nimsara <nsadisha@Hasinis-MacBook-Air.local>
2022-05-15 23:27:05 +0530

    Profile page added

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,9 +5,29 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'User Profile',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Container(
+      margin: EdgeInsets.only(top: 25),
+      child: Column(
+        children: [
+          Text("Profile", style: Theme.of(context).textTheme.headline4,),
+          SizedBox(height: 50,),
+          Container(
+            width: 150,
+            height: 150,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: NetworkImage("https://picsum.photos/250?image=9")
+                )
+            ),
+          ),
+          SizedBox(height: 30,),
+          Text("Sadisha Nimsara",style: Theme.of(context).textTheme.headline5),
+          Text("nsadisha@gmail.com", style: Theme.of(context).textTheme.bodyMedium,),
+          Expanded(child: Container(),),
+          ElevatedButton(onPressed: (){print("Signout");}, child: Text("Signout"))
+        ],
+      ),
     );
   }
 }
