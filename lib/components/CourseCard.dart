8af6b2d73736e9c72bb3ea311894e392cde45476
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-17 21:55:11 +0530

    Added class attributes

--- lib/components/CourseCard.dart
+++ lib/components/CourseCard.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class CourseCard extends StatelessWidget {
+
+    final int id;
+    final String courseName;
+    final String courseCode;
+    final String lecturerName;
+
+}
