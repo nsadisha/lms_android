092b4e5eb031c9b0dcf35c4029b4417445ca9ef0
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-17 22:11:28 +0530

    Made design color change according to course_id

--- lib/components/CourseCard.dart
+++ lib/components/CourseCard.dart
@@ -11,4 +11,28 @@ class CourseCard extends StatelessWidget {
         Key? key, required this.id, required this.courseName, required this.courseCode, required this.lecturerName,
     }) : super(key: key);
 
+    @override
+    Widget build(BuildContext context) {
+
+        Color c = Color((id * 1000000 * 3 * 0xFFFFFF).toInt()).withOpacity(1.0);
+
+        return Card(
+            clipBehavior: Clip.antiAlias,
+            child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                },
+                child: SizedBox(
+                    width: 350,
+                    height: 150,
+                    child: Column(
+                        children: [
+
+                        ],
+                    ),
+                ),
+            ),
+        );
+      
+    }
 }
