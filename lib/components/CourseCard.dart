de8aaaae09b1be46b0571ce73d4f4170818bcb29
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-17 21:59:15 +0530

    Added card design

--- lib/components/CourseCard.dart
+++ lib/components/CourseCard.dart
@@ -7,4 +7,8 @@ class CourseCard extends StatelessWidget {
     final String courseCode;
     final String lecturerName;
 
+    const CourseCard({
+        Key? key, required this.id, required this.courseName, required this.courseCode, required this.lecturerName,
+    }) : super(key: key);
+
 }
