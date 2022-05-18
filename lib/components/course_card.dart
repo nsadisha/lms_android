7d18eb0a2d5c9cd9c56f4bf63e58901b761b9d93
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-18 21:22:55 +0530

    Added padding section for the card

--- lib/components/course_card.dart
+++ lib/components/course_card.dart
@@ -2,71 +2,73 @@ import 'package:flutter/material.dart';
 
 class CourseCard extends StatelessWidget {
 
-    final int id;
-    final String courseName;
-    final String courseCode;
-    final String lecturerName;
+  final int id;
+  final String courseName;
+  final String courseCode;
+  final String lecturerName;
 
-    const CourseCard({
-        Key? key, required this.id, required this.courseName, required this.courseCode, required this.lecturerName,
-    }) : super(key: key);
+  const CourseCard({
+    Key? key, required this.id, required this.courseName, required this.courseCode, required this.lecturerName,
+  }) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
 
-        Color c = Color((id * 1000000 * 3 * 0xFFFFFF).toInt()).withOpacity(1.0);
+    Color c = Color((id * 1000000 * 3 * 0xFFFFFF).toInt()).withOpacity(1.0);
 
-        return Card(
-            clipBehavior: Clip.antiAlias,
-            child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                },
-                child: SizedBox(
-                    width: 350,
-                    height: 150,
-                    child: Column(
-                        children: [
-                            Align(
-                                alignment: Alignment.topLeft,
-                                child: SizedBox(
-                                    width: 250,
-                                    height: 36,
-                                    child: DecoratedBox(
-                                        decoration: BoxDecoration(
-                                            color: c,
-                                            borderRadius: const BorderRadius.only(
-                                                topLeft: Radius.zero,
-                                                topRight: Radius.zero,
-                                                bottomLeft: Radius.zero,
-                                                bottomRight: Radius.circular(40.0)
-                                            ),
-                                        ),
-                                    ),
-                                ),
-                            ),
-                            ListTile(
-                                title: Text(courseName),
-                                subtitle: Text(
-                                courseCode,
-                                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                                ),
-                            ),
-                            Align(
-                                alignment: Alignment.topLeft,
-                                child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                                    child: Text(
-                                        'Conducted by: $lecturerName',
-                                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                                    ),
-                                ),
-                            ),
-                        ],
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+          },
+          child: SizedBox(
+            width: 350,
+            height: 150,
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: SizedBox(
+                    width: 250,
+                    height: 36,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: c,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.zero,
+                            topRight: Radius.zero,
+                            bottomLeft: Radius.zero,
+                            bottomRight: Radius.circular(40.0)
+                        ),
+                      ),
                     ),
+                  ),
                 ),
+                ListTile(
+                  title: Text(courseName),
+                  subtitle: Text(
+                    courseCode,
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'Conducted by: $lecturerName',
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ),
+              ],
             ),
-        );
-      
-    }
+          ),
+        ),
+      ),
+    );
+  }
 }
