bec04cee28550638cf90d8fb446ddf65c240ae35
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-18 21:17:54 +0530

    Passed course details to card

--- lib/views/courses.dart
+++ lib/views/courses.dart
@@ -41,6 +41,17 @@ class _CoursesState extends State<Courses> {
                 itemCount: snapshot.data?.length,
                 itemBuilder: (BuildContext context, int index){
 
+                int id = snapshot.data!.elementAt(index).id;
+                String courseCode = snapshot.data!.elementAt(index).courseCode;
+                String courseName = snapshot.data!.elementAt(index).courseName;
+                String lecturerName = snapshot.data!.elementAt(index).lecturerName;
+
+                return CourseCard(
+                  id: id,
+                  courseCode: courseCode,
+                  courseName: courseName,
+                  lecturerName: lecturerName,
+                );
               });
         } else if (snapshot.hasError) {
           // return const EmptyState(text: "No announcements",);
