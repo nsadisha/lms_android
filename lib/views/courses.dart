b1e4ba699df96c64a671c545571ee2b60c25f36d
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-18 20:18:21 +0530

    Added the import files

--- lib/views/courses.dart
+++ lib/views/courses.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lms_android/models/Course.dart';
+import 'package:lms_android/service/CourseService.dart';
+import '../components/CourseCard.dart';
 
 class Courses extends StatelessWidget {
   const Courses({Key? key}) : super(key: key);
