b48a70903ab5bc96cfb9be74b6d7262eb8f2244e
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-18 20:21:55 +0530

    Added courses widget

--- lib/views/courses.dart
+++ lib/views/courses.dart
@@ -3,14 +3,9 @@ import 'package:lms_android/models/Course.dart';
 import 'package:lms_android/service/CourseService.dart';
 import '../components/CourseCard.dart';
 
-class Courses extends StatelessWidget {
+class Courses extends StatefulWidget {
   const Courses({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Text(
-      'All courses',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-    );
-  }
+  State<Courses> createState() => _CoursesState();
 }
