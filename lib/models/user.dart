66985cf18f47ba77a88474e3b44d6d90f064a46f
Sadisha Nimsara <nsadisha@gmail.com>
2022-05-20 00:12:52 +0530

    User JWT constructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,13 @@
 class User {
 
-  String email;
-  String password;
+  late String email;
+  late String password;
   late String name;
   late String role;
   late String cpass;
+  late List<String> courses;
 
   User(this.email, this.password);
   User.signup(this.email, this.password, this.name, this.role, this.cpass);
+  User.JWT(this.email, this.role, this.courses);
 }
