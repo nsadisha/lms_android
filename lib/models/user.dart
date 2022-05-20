403ca5814896387a562fdf6cce5eb1b5ca17f7b0
Hasini <hasinisama99@gmail.com>
2022-05-20 20:51:40 +0530

    user factory constructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -11,4 +11,9 @@ class User {
   User(this.email, this.password);
   User.signup(this.email, this.password, this.name, this.role, this.cpass);
   User.jwt(this.id, this.email, this.role, this.courses);
+  User.json(this.id, this.email, this.name);
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User.json(json['id'], json['email'], json['name']);
+  }
 }
