42f83bab4d88252505118aee7f4f5e7187bacbec
Hasini <hasinisama99@gmail.com>
2022-05-20 22:41:36 +0530

    student uneroll button

--- lib/views/course/student/details_tab_for_students.dart
+++ lib/views/course/student/details_tab_for_students.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:lms_android/models/course.dart';
 import 'package:lms_android/service/student_service.dart';
@@ -109,9 +111,74 @@ class _DetailsTabForStudentsState extends State<DetailsTabForStudents> {
                     return const CircularProgressIndicator();
                   }
               ),
-
             ],
-          )
+          ),
+          SizedBox(height: size.height * 0.05),
+
+          Container(
+            alignment: Alignment.bottomRight,
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    height: 10.0,
+                    width: size.width * 0.5,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        border: Border.all(width: 2.0, color: Colors.redAccent)
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 12),
+                  ),
+                  onPressed: () => showDialog<bool>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Center(child: Text('Unenroll')),
+                        content: Text("Are you sure you want to unenroll from the course module ${widget.course.courseName}?"),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, false),
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, true),
+                            child: const Text(
+                                'UNENROLL',
+                              style: TextStyle(
+                                  color: Colors.redAccent
+                              )
+                            ),
+                          ),
+                        ],
+                      ),
+                    ).then((exit) async {
+                    if (exit == null) return;
+                      if (exit) {
+                        var response = await studentService.unEnrollStudent(widget.userId, widget.course.id);
+                        if(response.statusCode == 200){
+                          log(response.statusCode.toString());
+                          // setState(() {});
+                        }else{
+                          log(response.statusCode.toString());
+                        }
+                      }
+                    }),
+                  child: const Text(
+                    "UNENROLL",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.redAccent
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
