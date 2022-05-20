0ca32b947346501eac981dd1268446ae15f8fc22
Hasini <hasinisama99@gmail.com>
2022-05-20 18:47:14 +0530

    Detail view for lecturers

--- lib/views/course/lecturer/details_tab_for_lecturers.dart
+++ lib/views/course/lecturer/details_tab_for_lecturers.dart
@@ -13,6 +13,68 @@ class _DetailsTabForLecturesState extends State<DetailsTabForLectures> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(widget.course.courseName));
+
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+        children: [
+          SizedBox(height: size.height * 0.03),
+          Text(
+            widget.course.courseName,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 24
+            ),
+          ),
+          Text(
+            widget.course.courseCode,
+            style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 16
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+          Text(
+            "Conducted by :  ${widget.course.lecturerName}",
+            style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16
+            ),
+          ),
+          SizedBox(height: size.height * 0.03),
+          Text(
+            widget.course.description,
+            style: const TextStyle(
+                color: Colors.black45,
+                fontSize: 14,
+            ),
+              textAlign: TextAlign.justify
+          ),
+          SizedBox(height: size.height * 0.05),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                  "Students Enrolled : ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+              ),
+              Text(
+                  "13",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 14,
+                  ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
