90792f1bcf76d1c99e2562f539f2937047d5d8c0
Hasini <hasinisama99@gmail.com>
2022-05-20 21:23:25 +0530

    get marks api call

---
+++ lib/service/student_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import '../baseURL.dart';
+import 'package:http/http.dart' as http;
+import 'local_storage_manager.dart';
+
+class StudentService {
+  final baseURL = BaseURL.url;
+  late final String _token;
+  static StudentService? _instance;
+
+  static Future<StudentService> getInstance() async {
+    if(_instance == null){
+      final lsm = await LocalStorageManager.getInstance();
+      _instance = StudentService._(await lsm.getToken());
+    }
+    return _instance!;
+  }
+
+  StudentService._(String token) : _token = token;
+
+  Future<double> getMarksForCourse(int studentId, int courseId) async {
+    final res = await http.get(
+      Uri.parse("$baseURL/student/$studentId/course/$courseId"),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $_token"
+      },
+    );
+    if (res.statusCode == 200) {
+      return json.decode(res.body);
+    } else {
+      throw "Unable to get enrolled students count";
+    }
+  }
+
+}
+
