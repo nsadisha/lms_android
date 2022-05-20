41f705a3350cc5a41cbb2bde0fc6796653215dbf
Sadisha Nimsara <nsadisha@gmail.com>
2022-05-20 14:07:36 +0530

    User id in constructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,6 @@
 class User {
 
+  late int id;
   late String email;
   late String password;
   late String name;
@@ -9,5 +10,5 @@ class User {
 
   User(this.email, this.password);
   User.signup(this.email, this.password, this.name, this.role, this.cpass);
-  User.JWT(this.email, this.role, this.courses);
+  User.jwt(this.id, this.email, this.role, this.courses);
 }
