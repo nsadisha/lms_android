04cc7ad595e4b6eb771c7c39f0c0b9be2adc8b4b
Hasini <hasinisama99@gmail.com>
2022-05-21 14:28:58 +0530

    Updated course details ui

--- lib/views/course_details.dart
+++ lib/views/course_details.dart
@@ -17,36 +17,38 @@ class _CourseDetailsState extends State<CourseDetails> {
   @override
   Widget build(BuildContext context) {
 
+    Size size = MediaQuery.of(context).size;
+
     final topContentText = Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.end,
       children: <Widget>[
-        const SizedBox(height: 120.0),
         const Icon(
-          Icons.directions_car,
+          Icons.menu_book,
           color: Colors.white,
-          size: 40.0,
+          size: 30.0,
         ),
         const SizedBox(
           width: 90.0,
-          child: Divider(color: Colors.green),
+          child: Divider(color: Colors.blue),
         ),
         const SizedBox(height: 10.0),
-        Text(
-          widget.course.courseName,
-          style: const TextStyle(color: Colors.white, fontSize: 45.0),
+        Flexible(
+          child: Text(
+            widget.course.courseName,
+            style: const TextStyle(color: Colors.white, fontSize: 35.0),
+          ),
         ),
-        const SizedBox(height: 30.0),
+        const SizedBox(height: 10.0),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Expanded(
                 flex: 6,
-                child: Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Text(
-                      widget.course.courseCode,
-                      style: const TextStyle(color: Colors.white),
-                    ))),
+                child: Text(
+                  widget.course.courseCode,
+                  style: const TextStyle(color: Colors.white, fontSize: 20.0),
+                )),
           ],
         ),
       ],
@@ -56,16 +58,18 @@ class _CourseDetailsState extends State<CourseDetails> {
       children: <Widget>[
         Container(
             padding: const EdgeInsets.only(left: 10.0),
-            height: MediaQuery.of(context).size.height * 0.5,
+            height: size.height * 0.5,
             decoration: const BoxDecoration(
               color: Color(0xff7c94b6),
             )
         ),
         Container(
-          height: MediaQuery.of(context).size.height * 0.5,
+          height: size.height * 0.5,
           padding: const EdgeInsets.all(40.0),
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
+          width: size.width,
+          decoration: const BoxDecoration(
+              color: Color.fromRGBO(58, 66, 86, .9),
+          ),
           child: Center(
             child: topContentText,
           ),
@@ -83,30 +87,68 @@ class _CourseDetailsState extends State<CourseDetails> {
       ],
     );
 
-    final bottomContentText = Text(
-      widget.course.description,
-      style: const TextStyle(fontSize: 14.0),
+    final bottomContentText = Column(
+      children: [
+        Text(
+          widget.course.description,
+          style: const TextStyle(color: Colors.black54, fontSize: 14.0),
+          textAlign: TextAlign.justify,
+        ),
+        const SizedBox(height: 30.0),
+        Text(
+          "Conducted by : ${widget.course.lecturerName}",
+          style: const TextStyle(fontSize: 16.0),
+          textAlign: TextAlign.left,
+        ),
+      ],
     );
+
     final readButton = Container(
-        padding: const EdgeInsets.symmetric(vertical: 16.0),
-        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        alignment: Alignment.center,
         child: Visibility(
           visible: widget.isStudent,
-          child: ElevatedButton(
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.fromLTRB(75.0,15.0,75.0,15.0),
-              primary: const Color.fromRGBO(58, 66, 86, 1.0),
-              // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-              textStyle: const TextStyle(fontSize: 14),
-            ),
-            onPressed: () => {},
-            child: const Text("Enroll", style: TextStyle(color: Colors.white)),
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: Container(
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: const LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 255, 136, 34),
+                            Color.fromARGB(255, 255, 177, 41)
+                          ]
+                      )
+                  ),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.fromLTRB(75.0,15.0,75.0,15.0),
+                  primary: Colors.white,
+                  textStyle: const TextStyle(fontSize: 14),
+                ),
+                onPressed: () async {
+
+                },
+                child: const Text(
+                  "ENROLL",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ],
           ),
         )
     );
+
     final bottomContent = Container(
-      // height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
+      // height: size.height,
+      width: size.width,
       // color: Theme.of(context).primaryColor,
       padding: const EdgeInsets.all(40.0),
       child: Center(
@@ -118,11 +160,13 @@ class _CourseDetailsState extends State<CourseDetails> {
     );
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          topContent,
-          bottomContent
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            topContent,
+            bottomContent
+          ],
+        ),
       ),
     );
   }
